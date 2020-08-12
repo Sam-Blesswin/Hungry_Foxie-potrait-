c8bacd6a0bc9fbf23148209ca88546124d66b26d
sam lucifer <samblesswinedu@gmail.com>
2020-08-13 02:05:43 +0530

    POTRAIT

--- Assets/scripts/swipe scripts/Charactermovement.cs
+++ Assets/scripts/swipe scripts/Charactermovement.cs
@@ -7,7 +7,7 @@ public class Charactermovement : MonoBehaviour
     public Playerinput swipecontrols;
     public Transform player;
     public float speed = 3f;
-    public float xrange = 3f;
+    public float xrange = 1f;
     private Vector3 desireddir;
 
 
@@ -19,11 +19,11 @@ public class Charactermovement : MonoBehaviour
     {
         if (swipecontrols.SwipeLeft || Input.GetKey(KeyCode.LeftArrow))
         {
-            desireddir += Vector3.left * 1.5f;
+            desireddir += Vector3.left * 1f;
         }
         else if (swipecontrols.SwipeRight || Input.GetKey(KeyCode.RightArrow))
         {
-            desireddir += Vector3.right *  1.5f;
+            desireddir += Vector3.right *  1f;
         }
         player.transform.position = Vector3.MoveTowards(player.transform.position, desireddir, speed * Time.deltaTime);
         desireddir.x = Mathf.Clamp(desireddir.x, -1 * xrange, xrange);
